791948585054f8f4dc7ff15a41f3efb29d218614
Gregg Meluski <gmeluski@hotmail.com>
2016-02-11 11:22:49 -0500

    something went wrong here

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  entry: getEntrySources(["./src/js/entry.js"]),
+  entry: getEntrySources(['./src/js/entry.js']),
   output: {
-    publicPath: "http://localhost:8080/",
-    filename: "build/bundle.js"
+    publicPath: 'http://localhost:8080/',
+    filename: 'build/bundle.js'
   },
-  devtool: "eval",
+  devtool: 'eval',
   module: {
     preLoaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components|test)/,
-        loader: "source-map"
+        exclude: /(node_modules|bower_components)/,
+        loader: 'source-map'
       }
     ],
     loaders: [
@@ -18,25 +18,25 @@ module.exports = {
         test: /\.scss$/,
         include: /src/,
         loaders: [
-          "style",
-          "css",
-          "autoprefixer?browsers=last 3 versions",
-          "sass?outputStyle=expanded"
+          'style',
+          'css',
+          'autoprefixer?browsers=last 3 versions',
+          'sass?outputStyle=expanded'
         ]
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          "url?limit=8192",
-          "img"
+          'url?limit=8192',
+          'img'
         ]
       },
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components|test)/,
+        exclude: /(node_modules|bower_components)/,
         loaders: [
-          "react-hot",
-          "babel?presets[]=stage-0,presets[]=react,presets[]=es2015"
+          'react-hot',
+          'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
         ]
       }
     ]
@@ -44,9 +44,9 @@ module.exports = {
 };
 
 function getEntrySources(sources) {
-  if (process.env.NODE_ENV !== "production") {
-    sources.push("webpack-dev-server/client?http://localhost:8080");
-    sources.push("webpack/hot/only-dev-server");
+  if (process.env.NODE_ENV !== 'production') {
+    sources.push('webpack-dev-server/client?http://localhost:8080');
+    sources.push('webpack/hot/only-dev-server');
   }
 
   return sources;
